1e8809af1aa8f5d1eff1c9efa0dac7b23b8fab49
Kodux78k <koduxlive@gmail.com>
2026-02-28 02:01:05 -0300

    Atualizar o kobdh0-main.js

--- M0D/kob-DH0/js/kobdh0-main.js
+++ M0D/kob-DH0/js/kobdh0-main.js
@@ -1,17 +1,3 @@
-import {
-} from './m0ds/kob-glue-dh10-4.js';
-
-
-import {
-  registerArchetypes,
-  activateArchetype,
-  speakWithCurrentArchetype
-} from './m0ds/kob-voice-engine-0.js';
-
-registerArchetypes(ARCHETYPES);
-
-// troca arquétipo
-activateArchetype('atlas');
-
-// fala sincronizada
-speakWithCurrentArchetype('O sistema está vivo.');
+import {} from './m0ds/kob-glue-dh10-4.js';
+import { registerArchetypes, activateArchetype, speakWithCurrentArchetype } 
+  from './m0ds/kob-voice-engine-0.js';
