d72fc550a82103340637a40f60f4194525fd58c2
Kodux78k <koduxlive@gmail.com>
2026-03-07 07:21:37 -0300

    Atualizar o kobdh0-main.js

--- M0D/kob-DH0/js/kobdh0-main.js
+++ M0D/kob-DH0/js/kobdh0-main.js
@@ -1,10 +1,10 @@
 import {} from './m0ds/kob-glue-dh10-2-oai.js';
-import {} from './m0ds/kob-hud-aa.js';
+//* import {} from './m0ds/kob-hud-aa.js';
 import {
   registerArchetypes,
   activateArchetype,
   speakWithCurrentArchetype
-} from './m0ds/kob-voice-engine-0.js';
+} from './m0ds/kob-voice-engine-1.js';
 
 registerArchetypes(ARCHETYPES);
 activateArchetype('atlas');
