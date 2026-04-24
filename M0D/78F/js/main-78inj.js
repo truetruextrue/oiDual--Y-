32871d9ed12bce517d79c5bcce56e486c23c1ac9
Kodux78k <koduxlive@gmail.com>
2026-04-24 00:22:22 -0300

    Create main-78inj.js

---
+++ M0D/78F/js/main-78inj.js
@@ -0,0 +1,7 @@
+console.log("Infodose conectado", {
+    ts: Date.now(),
+    id: crypto.randomUUID(),
+    meta: { app: "generated.app" }
+  });
+
+  import "https://kodux78k.github.io/oiDual--Y-/M0D/78F/js/main.js";
