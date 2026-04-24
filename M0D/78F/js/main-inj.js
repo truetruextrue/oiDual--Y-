8365f0aacb67102e6cacfda1738d2efbba47bfd0
Kodux78k <koduxlive@gmail.com>
2026-04-24 00:13:51 -0300

    Create main-inj.js

---
+++ M0D/78F/js/main-inj.js
@@ -0,0 +1,4 @@
+console.log("Infodose conectado", {"ts":1776989330826,"id":"5c9a2887-8bfb-4d85-9522-d7ac2430aa34","meta":{"app":"generated.app"}});
+import ".../css/main.css"; //css
+import "./modules/78F-m0d-inj.js"; //html
+import "./modules/inline-0.js"; //script
