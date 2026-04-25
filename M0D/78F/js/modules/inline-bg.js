15d788d8b9af0b6070c83ac42baac81d3e7e3231
Kodux78k <koduxlive@gmail.com>
2026-04-25 02:21:24 -0300

    Criar inline-bg.js

---
+++ M0D/78F/js/modules/inline-bg.js
@@ -0,0 +1,10 @@
+function loadBackgroundFromStorage() {
+  const url = localStorage.getItem('di_bg_url');
+  const opacity = localStorage.getItem('di_bg_opacity');
+  const blend = localStorage.getItem('di_bg_blend');
+  if (url && url !== 'null') {
+    applyBackgroundToDOM(url, opacity || 15, blend || 'overlay');
+    // sincroniza UI da drawer
+  }
+}
+// Executado no DOMContentLoaded e no message do iframe
